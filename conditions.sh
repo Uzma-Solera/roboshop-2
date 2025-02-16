df7a74814b5c3a47068b4a5ec95eb031eeed9e87
uzmaa <uzmaashfaq.shavsi@gmail.com>
2025-02-16 19:52:38 +0530

    This is my first version.

---
+++ conditions.sh
@@ -0,0 +1,9 @@
+x = 10
+
+if [ $x -gt 10]
+then
+  echo "$x is greater then 10"
+else
+  echo "$x is less then 10"
+fi
+
--- mongodb.sh
+++
@@ -1,7 +0,0 @@
-
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
-dnf install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
-sid -i
-systemctl restart mongod
