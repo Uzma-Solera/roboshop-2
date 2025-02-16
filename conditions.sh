b30fb0f67eec0cb1866e73a4070d40cfe6b5cb97
uzmaa <uzmaashfaq.shavsi@gmail.com>
2025-02-16 19:55:53 +0530

    This is my 2nd version

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,8 @@
 x = 10
 
-if [ $x -gt 10]
-then
-  echo "$x is greater then 10"
+if [ $x -gt10]; then
+  echo"$x is greater then 10"
 else
-  echo "$x is less then 10"
+  echo"$x is less then 10"
 fi
 
